29480a32853d74f61fe8979db31e7388801db287
NgMingZan <110454814+NgMingZan@users.noreply.github.com>
2026-09-20 07:27:10 +0700

    build: bump app to V3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.m4x.themestudio"
         minSdk = 29
         targetSdk = 35
-        versionCode = 8
-        versionName = "3.1.0"
+        versionCode = 9
+        versionName = "3.2.0"
     }
 
     buildFeatures {
